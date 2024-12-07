8f8a6d2006dc990aac81af27ed05f2cef8de925f
Nixixouwu <ni.aliaga@duocuc.cl>
2024-12-07 17:40:14 -0300

    Fix

--- src/environments/environment.ts
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyBaq7mwDUY8x2UeZh32dhl-B4v3-WU7HQQ",
-    authDomain: "abyss-requester.firebaseapp.com",
-    projectId: "abyss-requester",
-    storageBucket: "abyss-requester.firebasestorage.app",
-    messagingSenderId: "2160297989",
-    appId: "1:2160297989:web:0e41f0b0ecfcd78049f46c"
-  };
