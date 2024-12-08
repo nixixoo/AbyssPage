5dfc1a62d0c6177ea5b8022e1fef96983e023604
Nixixouwu <ni.aliaga@duocuc.cl>
2024-12-08 16:08:15 -0300

    home & header phone

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit, OnDestroy } from '@angular/core';
+import { Component, Input, OnInit, OnDestroy, PLATFORM_ID, Inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
 import { Subject } from 'rxjs';
 import { takeUntil } from 'rxjs/operators';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
   selector: 'app-header',
@@ -17,11 +18,19 @@ export class HeaderComponent implements OnInit, OnDestroy {
   @Input() darkened: boolean = false;
   isLoggedIn: boolean = false;
   private destroy$ = new Subject<void>();
+  isMenuOpen: boolean = false;
+  isMobile: boolean = false;
 
   constructor(
     private router: Router,
-    private authService: AuthService
-  ) {}
+    private authService: AuthService,
+    @Inject(PLATFORM_ID) private platformId: Object
+  ) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.checkIfMobile();
+      window.addEventListener('resize', () => this.checkIfMobile());
+    }
+  }
 
   ngOnInit() {
     this.authService.currentUser$
@@ -34,26 +43,44 @@ export class HeaderComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.destroy$.next();
     this.destroy$.complete();
+    if (isPlatformBrowser(this.platformId)) {
+      window.removeEventListener('resize', () => this.checkIfMobile());
+    }
+  }
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
   }
 
   navigateToLogin() {
+    this.isMenuOpen = false;
     this.router.navigate(['/login']);
   }
 
   navigateToCharacters() {
+    this.isMenuOpen = false;
     this.router.navigate(['/characters']);
   }
 
   navigateToTeamSee() {
+    this.isMenuOpen = false;
     this.router.navigate(['/team-see']);
   }
 
   navigateToRegister() {
+    this.isMenuOpen = false;
     this.router.navigate(['/register']);
   }
 
   navigateToFeedback() {
+    this.isMenuOpen = false;
     this.router.navigate(['/feedback']);
+  }
+
+  private checkIfMobile() {
+    if (isPlatformBrowser(this.platformId)) {
+      this.isMobile = window.innerWidth <= 768;
+    }
   }
 }
 
