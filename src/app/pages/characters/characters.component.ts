ed5f54953fe0a49ee7532c3bbaa7881e82de0229
Nixixouwu <ni.aliaga@duocuc.cl>
2025-02-28 19:43:51 -0300

    Action Buttons Characters

--- src/app/pages/characters/characters.component.ts
+++ src/app/pages/characters/characters.component.ts
@@ -16,10 +16,13 @@ import { characterList } from '../../constants/character-list';
   templateUrl: './characters.component.html',
   styleUrls: ['./characters.component.scss'],
   animations: [
-    trigger('fadeOut', [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({ opacity: 0 }),
+        animate('300ms ease-in', style({ opacity: 1 }))
+      ]),
       transition(':leave', [
-        style({ opacity: 1 }),
-        animate('0.5s ease-out', style({ opacity: 0 }))
+        animate('300ms ease-out', style({ opacity: 0 }))
       ])
     ])
   ]
